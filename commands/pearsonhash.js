d6692f83bdb5148d35254294c743a9df1acb5cf0
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2020-07-18 01:02:44 -0300

    Minor cleanup in pearsonhash

--- commands/pearsonhash.js
+++ commands/pearsonhash.js
@@ -1,18 +1,20 @@
 const { fisherYates } = require('../utils/fisherYates.js');
 const { range } = require('../utils/graphtools.js');
 
-// generate the table for pearson's hash
-const mytable = fisherYates(range(256));
-
 // from a permutation table, generate a function that performs a step
 // of pearson's hash
 const pearson = table => (h, x) => table[h ^ x];
 
+// generate the table for pearson's hash
+const mytable = fisherYates(range(256));
+const mypearson = pearson(mytable);
+
 const execute = (msg, argstring) => {
-  const data = argstring.match(/"(?<data>.*)"/).groups.data;
-  const mypearson = pearson(mytable);
-  const codes = data.split('').map(s => s.charCodeAt(0));
-  const hash = codes.reduce(mypearson, 0);
+  const { data } = argstring.match(/"(?<data>.*)"/).groups;
+  const hash = data
+    .split('')
+    .map(s => s.charCodeAt(0))
+    .reduce(mypearson, 0);
   msg.reply(`"${data}" hashed to ${hash} with table [${mytable.join(' ')}]`);
 };
 
